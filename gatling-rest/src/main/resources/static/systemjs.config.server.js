7fa906fd68845b77fc315e29e0a04e4a664b4135
Stuart Saltzman <stuart.saltzman@gmail.com>
2018-05-11 10:32:38 -0400

    chore_NO-REF_fix_angular_deps

--- gatling-rest/src/main/resources/static/systemjs.config.server.js
+++ gatling-rest/src/main/resources/static/systemjs.config.server.js
@@ -13,18 +13,20 @@
       // our app is within the app folder
       app: 'app',
       // angular bundles
-      '@angular/core': 'npm:@angular/core/bundles/core.umd.js',
-      '@angular/common': 'npm:@angular/common/bundles/common.umd.js',
-      '@angular/compiler': 'npm:@angular/compiler/bundles/compiler.umd.js',
-      '@angular/platform-browser': 'npm:@angular/platform-browser/bundles/platform-browser.umd.js',
-      '@angular/platform-browser-dynamic': 'npm:@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js',
-      '@angular/http': 'npm:@angular/http/bundles/http.umd.js',
-      '@angular/router': 'npm:@angular/router/bundles/router.umd.js',
-      '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.js',
-      '@angular/upgrade': 'npm:@angular/upgrade/bundles/upgrade.umd.js',
+      '@angular/core': 'npm:@angular/core@2.4.9/bundles/core.umd.js',
+      '@angular/common': 'npm:@angular/common@2.4.9/bundles/common.umd.js',
+      '@angular/compiler': 'npm:@angular/compiler@2.4.9/bundles/compiler.umd.js',
+      '@angular/platform-browser': 'npm:@angular/platform-browser@2.4.9/bundles/platform-browser.umd.js',
+      '@angular/platform-browser-dynamic': 'npm:@angular/platform-browser-dynamic@2.4.9/bundles/platform-browser-dynamic.umd.js',
+      '@angular/http': 'npm:@angular/http@2.4.9/bundles/http.umd.js',
+      '@angular/router': 'npm:@angular/router@3.4.9/bundles/router.umd.js',
+      '@angular/forms': 'npm:@angular/forms@2.4.9/bundles/forms.umd.js',
+      '@angular/upgrade': 'npm:@angular/upgrade@2.4.9/bundles/upgrade.umd.js',
       // other libraries
-      'rxjs':                      'npm:rxjs@5.4.1',
-      'angular-in-memory-web-api': 'npm:angular-in-memory-web-api/bundles/in-memory-web-api.umd.js'
+      //'rxjs':                      'npm:rxjs@5.4.1',
+      //'rxjs-forward-compat':        'npm:rxjs-forward-compat@6.0.0',
+      'rxjs':                      'npm:rxjs@5.5.4',
+      'angular-in-memory-web-api': 'npm:angular-in-memory-web-api@2.4.9/bundles/in-memory-web-api.umd.js'
     },
     // packages tells the System loader how to load when no filename and/or no extension
     packages: {
